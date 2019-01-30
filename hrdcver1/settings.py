eeb996fc0976bf513b82db26104dfced437969ea
Aayush <skdeepak15@gmail.com>
2019-01-30 19:27:45 +0530

    Updated Settings.py
    Test

--- hrdcver1/settings.py
+++ hrdcver1/settings.py
@@ -73,15 +73,14 @@ WSGI_APPLICATION = 'hrdcver1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,7 +94,7 @@ DATABASES = {
         },  
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
